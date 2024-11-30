9d398928307479d738cc43d8de28a35af636481f
skyfire <skyfireitdiy@hotmail.com>
2024-11-30 21:36:13 +0800

    升级版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.jianpian"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.2"
     }
 
     buildTypes {
